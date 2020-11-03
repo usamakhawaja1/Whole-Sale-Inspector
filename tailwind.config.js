596284d8a8b8b530a0f9228998dba02ffdbd6865
Usama khawaja <47067489+usamakhawaja1@users.noreply.github.com>
2020-11-03 16:28:01 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        extend: {
+            brand: {
+                headerblue: "#0C1D40",
+                headerbtnclr1: "#2D3A4B",
+                headerbtnclr2: "#0278AE",
+                grayclr: "#B2B0B0",
+                fclr: "#828283",
+                flink: "#A9ADC1"
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
